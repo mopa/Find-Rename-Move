9867d103d2fd34e6f22f265d4e6fb77604d148c6
Pablo M <951188+mopa@users.noreply.github.com>
2019-02-11 22:21:25 +0100

    A functional script

--- move.sh
+++ move.sh
@@ -1,15 +1,37 @@
 #!/usr/bin/env bash
+#################################
+# FIND-RENAME-MOVE
+#
+# VERSION 0.1
+# Author: Pablo M. Pareja
+#################################
 
-DIR="tosjuntos"
+DIR="Videos"
 mkdir $DIR
+read -p "File Extension: " IEXT
 
-find . \( -iname '*.txt'  \) -type f -exec mv -nv -t $DIR/ -- {} +
+# Find all files in the directory and subdir
+find . \( -iname "*.$IEXT" \) -type f -exec mv -nv -t $DIR/ -- {} +
 
+# Move to dir that contain the files found and
+# input the info
 cd $DIR
 
-a=1
-for i in *.txt; do
-  new=$(printf "Twin s01e%0d.txt" "$a") #04 pad to length of 4
+read -p "Nombre : " SNAME
+read -p "Temporada: " SNUM
+read -p "Capitulo Inicial: " ICAP
+
+# Start Loop!!!!
+a=$ICAP
+for i in *.$IEXT; do
+  new=$(printf "$SNAME-s"$SNUM"e%0d.txt" "$a")
   mv -i -- "$i" "$new"
   let a=a+1
 done
+
+# Show me the content and move to other dir
+ls -lah .
+
+read -p "Directorio destino final: " FDIR
+mv *.$IEXT $FDIR/
+
